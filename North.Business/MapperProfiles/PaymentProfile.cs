94bf2b7150adf31ef1d0a8f8f237849d4658e48b
AhmetKbpc <resmi.ahmetk@gmail.com>
2022-06-01 14:30:06 +0300

    Iyzipay Models, ApiOptions, PaymentProfile add OK

---
+++ North.Business/MapperProfiles/PaymentProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Iyzipay.Model;
+using North.Core.Payments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace North.Business.MapperProfiles
+{
+    public class PaymentProfile :Profile
+    {
+        public PaymentProfile()
+        {
+            CreateMap<CardModel , PaymentCard>().ReverseMap();
+            CreateMap<BasketModel , BasketItem>().ReverseMap();
+            CreateMap<AddressModel , Address>().ReverseMap();
+            CreateMap<CustomerModel , Buyer>().ReverseMap();
+            CreateMap<InstallmentPriceModel , InstallmentPrice>().ReverseMap();
+            CreateMap<InstallmentModel , InstallmentDetail>().ReverseMap();
+            CreateMap<PaymentResponseModel , Payment>().ReverseMap();
+        }
+    }
+}
